32ec089066ffc31f976f33909ee4ddfcc22d2a6a
nmpestcontrol <chronicpainrewired@gmail.com>
2025-10-08 19:28:21 -0600

    init with absolute imports.

--- src/asimov/__init__.py
+++ src/asimov/__init__.py
@@ -1,16 +1,16 @@
 """Asimov: A package for simulating Asimovian Economic Theory using agent-based modeling."""
-from .models.asimovian import Asimovian
-from .agents.network.isaac import Isaac
-from .agents.network.bondholder_set import BondholderSet
-from .agents.network.bidnet import BidNet
-from .agents.network.daneel import Daneel
-from .agents.network.robot_set import RobotSet
-from .agents.network.enterprise_set import EnterpriseSet
-from .agents.network.giskard import Giskard
-from .agents.network.robofund import RoboFund
-#from .agents.edges.mint_request import MintRequest
-from .agents.edges.edge_set import EdgeSet
-from .agents.edges.customer_edges import CustomerEdges
+from asimov.models.asimovian import Asimovian
+from asimov.agents.network.isaac import Isaac
+from asimov.agents.network.bondholder_set import BondholderSet
+from asimov.agents.network.bidnet import BidNet
+from asimov.agents.network.daneel import Daneel
+from asimov.agents.network.robot_set import RobotSet
+from asimov.agents.network.enterprise_set import EnterpriseSet
+from asimov.agents.network.giskard import Giskard
+from asimov.agents.network.robofund import RoboFund
+#from asimov.agents.edges.mint_request import MintRequest
+from asimov.agents.edges.edge_set import EdgeSet
+from asimov.agents.edges.customer_edges import CustomerEdges
 
 
 
